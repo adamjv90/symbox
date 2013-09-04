031818c06433bc61b388257bb1278611ca952eef
Simon Schröer <ssc@basecom.de>
2013-09-04 17:31:32 +0200

    Installation via symlink and documentation update

--- ComposerCallback.php
+++ ComposerCallback.php
@@ -4,48 +4,21 @@ namespace basecom\symbox;
 
 class ComposerCallback
 {
-    protected static function copy($src, $dest)
-    {
-        if(\is_file($src)) {
-            if(\file_exists($dest)) {
-                \rename($dest, $dest.'.bak');
-            }
-            return \copy($src, $dest);
-        }
-
-        foreach(\scandir($src) as $file)
-        {
-            $tmpSrc = $src.'/'.$file;
-            $tmpDest = $dest.'/'.$file;
-
-            if(!\is_readable($tmpSrc) || \in_array($file, array('.', '..'))) {
-                continue;
-            }
-
-            if(\is_dir($tmpSrc)) {
-                if(!\is_dir($tmpDest)) {
-                    \mkdir($tmpDest, 755, true);
-                }
-                return self::copy($tmpSrc, $tmpDest);
-            }
-            else {
-                if(\file_exists($tmpDest)) {
-                    \rename($tmpDest, $tmpDest.'.bak');
-                }
-                return \copy($tmpSrc, $tmpDest);
-            }
-        }
-
-        return false;
-    }
-
     public static function install()
     {
+        // get the source and target dir
         $sourceDir = \realpath(__DIR__);
         $targetDir = \realpath(\sprintf("%s/../../../", $sourceDir));
 
-        self::copy($sourceDir.'/symbox', $targetDir.'/symbox');
-        self::copy($sourceDir.'/Vagrantfile', $targetDir.'/Vagrantfile');
+        // install symlink
+        $cmd = ('win' === \strtolower(\PHP_OS) ? "mklink %s/symbox %s/symbox" : "ln -s %s/symbox %s/symbox");
+        \passthru(\sprintf($cmd, $sourceDir, $targetDir));
+
+        // copy the vagrant file
+        if(\file_exists($targetDir.'/Vagrantfile')) {
+            \copy($targetDir.'/Vagrantfile', $targetDir.'/Vagrantfile.bak');
+        }
+        \copy($sourceDir.'/Vagrantfile', $targetDir.'/Vagrantfile');
     }
 
     public static function update()
